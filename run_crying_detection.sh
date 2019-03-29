2bffe81fcb8140bff120760f26d9b30f6eed9d90
Phuong Duong <dthphuong94@live.com>
2019-03-29 14:44:34 +0700

    modify project for RPi

--- run_crying_detection.sh
+++ run_crying_detection.sh
@@ -18,22 +18,22 @@ trap clean_up SIGHUP SIGINT SIGTERM
 
 function recording(){
 	echo "Start Recording..."
-	arecord -D plughw:1,0 -d 9 -f S16_LE -c1 -r44100 -t wav /opt/baby_cry_rpi/recording/signal_9s.wav
+	arecord -D plughw:0 -d 9 -f S16_LE -c1 -r44100 -t wav /baby_cry_detection/recording/signal_9s.wav
 }
 
 
 function predict() {
 	echo "Predicting..."
 	echo -n "What is the prediction ?"
-	python /opt/baby_cry_rpi/script/make_prediction.py
-	PREDICTION=$(cat /opt/baby_cry_rpi/prediction/prediction.txt)
+	python /baby_cry_detection/baby_cry_detection/rpi_main/make_prediction.py
+	PREDICTION=$(cat /baby_cry_detection/prediction/prediction.txt)
 	echo "Prediction is $PREDICTION"
 }
 
 function start_playing() {
 	if [[ $PLAYING == 0 ]]; then
 		echo "start playing"
-                aplay -D plughw:0,0 /opt/baby_cry_rpi/lullaby/lullaby_classic.wav
+                aplay -D plughw:0 /baby_cry_detection/lullaby/lullaby_classic.wav
 		PLAYING=1
 	fi
 }
